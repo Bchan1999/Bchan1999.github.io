b8ab8bb095d487de44c6297568e6cb1098af8c90
Bchan1999 <brandon.nic99@gmail.com>
2022-09-07 20:43:51 -0600

    Added a header template component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header.js'
 
 function App() {
+
+
   return (
     <div className="App">
+      <Header/>
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = (props) => {
+    return (
+        <div>
+            <a href='#' >Home</a>
+        </div>
+    )
+}
+
+export default Header;
