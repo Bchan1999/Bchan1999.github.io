e423e43e2e598c13d8f40eee29667e0c38fb2c44
Bchan1999 <brandon.nic99@gmail.com>
2022-09-08 23:23:43 -0600

    Basic nav bar template

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
 
   return (
     <div className="App">
-      <Header/>
+      
       <header className="App-header">
-        
-        <img src={logo} className="App-logo" alt="logo" />
+      <Header/>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,7 +21,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './../Header.css'
 
 const Header = (props) => {
     return (
         <div>
             <a href='#' >Home</a>
+            <div id="menu">
+                <a href="#">Shop</a>
+                <a href="#">About</a>
+                <a href="#">Contact Us</a>
+            </div>
         </div>
     )
 }
