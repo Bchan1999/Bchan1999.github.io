dbede51eee084764fd840567ba5df3d376716761
Bchan1999 <brandon.nic99@gmail.com>
2022-10-15 09:13:54 -0600

    About component template created

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,7 +4,17 @@ const About = (props) => {
     return (
         <>
             <div>
-                
+                <ul className="infobar">
+                    <li><span>1277</span>Sales</li>
+                    <li><span>5.0</span>Average star reviews</li>
+                    <li><span>Over 200</span>Needle minders</li>
+                </ul>
+                <div className="aboutme">
+                    <img src="#"></img>
+                    <h2>Who am I?</h2>
+                    <p>My name is Linda Chan and I am a part time  mother and part time crosstching enthusasit.  I wanted to see more cute and humrous neelde minders in stores. So in 2021 I decided to turn my passion into a business. </p>
+                </div>
+
             </div>
         </>
     )
