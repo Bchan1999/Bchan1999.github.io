c3806fc979d271045b16409ae4dee8276a490c28
Bchan1999 <brandon.nic99@gmail.com>
2023-01-06 14:21:57 -0700

    Rough format for about me page

--- src/Components/About.js
+++ src/Components/About.js
@@ -16,21 +16,20 @@ const About = (props) => {
                     <li><span>Over 200</span><br/>Needle minders</li>
                 </ul>
 
-                <div className="about-me-pic">
+                <div className="bottom-div">
                    <div class="bottom-wave">
                         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                         <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
                         </svg>
                     </div>  
-                    
-                 <img src="#"></img>
-                 
                 </div>
-                
-                <div className="aboutme">
-                    <h2>Who am I?</h2>
-                    <p>My name is Linda Chan and I am a part time  mother and part time crosstching enthusasit.  I wanted to see more cute and humrous neelde minders in stores. So in 2021 I decided to turn my passion into a business. </p>
-                </div>
+                <div className="bio">
+                    <img id='face' src={require('../images/mom.png')}></img>
+                    <div className="aboutme">
+                        <h2>Who am I?</h2>
+                        <p>My name is Linda Chan and I am a part time mother and part time crosstching enthusiast.  I wanted to collect more cute and humorous needle minders for my crosstching projects, but they were hard to find. So in 2021 I decided to change that and now I have turned this obession into a business. </p>
+                    </div>
+                </div> 
                 
             </div>
 
--- src/Components/Title.js
+++ src/Components/Title.js
@@ -3,14 +3,25 @@ import '../Style/Title.css'
 import {Link} from 'react-scroll'
 
 const Title = (props) => {
+
+    function etsyLink(e) {
+        console.log('test')
+    }
+
     return (
         <div className = "hero">
+            <div id="shop-link" onClick={etsyLink}>
+                <div id="link">
+                <a href='https://www.etsy.com/ca/shop/TheNeedleNanny?ref=simple-shop-header-name&listing_id=1159865656'>Find me on</a>
+                <img src={require('../images/etsy.png')}></img>
+                </div>
+            </div> 
             <div className = "hero-body">
                 <h1 className = "title">The Needle Nanny</h1>
                 <p>Committed to giving you high-quality needle minders for all your needle art needs.</p>
             </div>
-            <div><img src={require("../images/logo.png")}/></div>
-            <a href='https://www.etsy.com/ca/shop/TheNeedleNanny?ref=simple-shop-header-name&listing_id=1159865656'>Find me on Etsy</a>
+            <div><img src=""/></div>
+
                 <div class="top-wave">
                     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
